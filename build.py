592326d00afacd1183cb7fae11281388dee550db
Ciremun <Ciremun@yandex.ru>
2021-04-22 13:23:13 +0300

    build_all_with_extension

--- build.py
+++ build.py
@@ -5,14 +5,16 @@ import traceback
 import sys
 import os
 
-from typing import Callable, Any
+from typing import Callable, Any, Optional
 
 # TODO(#1): build from args
 CC = os.environ.get('CC')
 CFLAGS = os.environ.get('CFLAGS')
 RC = 'rustc'
 
+SOURCES = os.listdir('.')
 build_functions = {}
+
 
 def build_function(ext: str) -> Callable:
     def decorator(func: Callable) -> Callable:
@@ -23,6 +25,7 @@ def build_function(ext: str) -> Callable:
         return wrapper
     return decorator
 
+
 def catch_errors(func: Callable) -> Callable:
     def wrapper(*args, **kwargs) -> Any:
         try:
@@ -32,23 +35,30 @@ def catch_errors(func: Callable) -> Callable:
             traceback.print_exc()
     return wrapper
 
+
 def log_error(err: str):
     print(f'ERROR: {err}')
 
-def build_source(src: str):
+
+def extract_source_extension(src: str) -> Optional[str]:
     ext = ''
     for char in src[::-1]:
         if char == '.':
-            ext = ext[::-1]
-            bf = build_functions.get(ext)
-            if bf is not None:
-                bf(src)
-            else:
-                log_error(f'`{src}` unknown file extension: `.{ext}`')
-            break
+            return ext[::-1]
         ext += char
-    else:
-        log_error(f'source `{src}` has no extension')
+    log_error(f'source `{src}` has no extension')
+    return None
+
+
+def build_source(src: str):
+    ext = extract_source_extension(src)
+    if ext:
+        bf = build_functions.get(ext)
+        if bf:
+            bf(src)
+        else:
+            log_error(f'`{src}` unknown file extension: `.{ext}`')
+
 
 @build_function('c')
 def build_c_source(src: str):
@@ -63,23 +73,39 @@ def build_c_source(src: str):
         command.extend((f'-o{filename}',))
     run_command(command)
 
+
 @build_function('rs')
 def build_rust_source(src: str):
     run_command([RC, src])
+
 
 @catch_errors
 def run_command(command: str):
     print(' '.join(command))
     subprocess.run(command)
 
+
 def build_all():
-    SOURCES = os.listdir('.')
     CSOURCES = [f for f in SOURCES if f.endswith('.c')]
     RSOURCES = [f for f in SOURCES if f.endswith('.rs')]
     for src in CSOURCES:
         build_c_source(src)
     for src in RSOURCES:
         build_rust_source(src)
+
+
+def build_all_with_extension(src: str):
+    ext = extract_source_extension(src)
+    if ext:
+        bf = build_functions.get(ext)
+        if bf:
+            sources_with_extension = [
+                s for s in SOURCES if s.endswith(f'.{ext}')]
+            for s in sources_with_extension:
+                bf(s)
+        else:
+            log_error(f'`{src}` unknown file extension: `.{ext}`')
+
 
 if __name__ == '__main__':
     if sys.platform == 'win32':
@@ -91,7 +117,9 @@ if __name__ == '__main__':
         CC = 'gcc'
     if len(sys.argv) > 1:
         for src in sys.argv[1:]:
-            build_source(src)
+            if src.startswith('*.'):
+                build_all_with_extension(src)
+            else:
+                build_source(src)
     else:
         build_all()
-
