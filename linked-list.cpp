2c540cba555de6ef688ce12525174e6406fba2ae
Ciremun <Ciremun@yandex.ru>
2021-04-23 17:33:17 +0300

    ll init

--- linked-list.cpp
+++ linked-list.cpp
@@ -1,7 +1,39 @@
 #include <stdio.h>
 
+class Node
+{
+public:
+    const char *data;
+    Node *next;
+
+    void print()
+    {
+        Node *node = this;
+        while (node != NULL)
+        {
+            printf("%s ", node->data);
+            node = node->next;
+        }
+        printf("\n");
+    }
+};
+
 int main()
 {
-    printf("Hello world!\n");
+    Node *head = new Node();
+    Node *second = new Node();
+    Node *third = new Node();
+
+    head->data = "head";
+    head->next = second;
+
+    second->data = "second";
+    second->next = third;
+
+    third->data = "third";
+    third->next = NULL;
+
+    head->print();
+
     return 0;
 }
