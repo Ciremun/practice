c91de17ef278c7a8f65dfc1ccf3756de97cd4713
Ciremun <Ciremun@yandex.ru>
2021-04-23 23:07:04 +0300

    singly linked list: remove

--- linked-list.cpp
+++ linked-list.cpp
@@ -51,6 +51,7 @@ public:
         new_node->next = head;
         head = new_node;
     }
+
     void insert(Node<T> *prev, T data)
     {
         Node<T> *new_node = new Node<T>();
@@ -58,6 +59,7 @@ public:
         new_node->next = prev->next;
         prev->next = new_node;
     }
+
     void append(T data)
     {
         Node<T> *new_node = new Node<T>();
@@ -70,14 +72,41 @@ public:
         }
         last->next = new_node;
     }
+
+    void remove(T data)
+    {
+        Node<T> *temp = head;
+        if (temp->data == data)
+        {
+            head = temp->next;
+            delete temp;
+            return;
+        }
+        Node<T> *prev = NULL;
+        while (temp != NULL && temp->data != data)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+        if (temp == NULL)
+        {
+            return;
+        }
+        prev->next = temp->next;
+        delete temp;
+    }
 };
 
 int main()
 {
-    SinglyLinkedList<int> int_list = SinglyLinkedList<int>(69, 420);
+    SinglyLinkedList<int> int_list = SinglyLinkedList<int>(69, 420, 1337);
+    int_list.remove(420);
+    int_list.remove(1337);
     int_list.head->print();
 
-    SinglyLinkedList<const char *> str_list = SinglyLinkedList<const char *>("dungeon", "master");
+    SinglyLinkedList<string> str_list = SinglyLinkedList<string>("master", "dungeon");
+    str_list.remove("master");
+    str_list.append("master");
     str_list.head->print();
 
     return 0;
