9f9953acf4c6104998f6d86335e4ba38ed0561bd
Ciremun <Ciremun@yandex.ru>
2021-04-23 22:34:48 +0300

    Singly Linked List init

--- linked-list.cpp
+++ linked-list.cpp
@@ -1,39 +1,84 @@
-#include <stdio.h>
+#include <iostream>
 
+using namespace std;
+
+template <typename T>
 class Node
 {
 public:
-    const char *data;
+    T data;
     Node *next;
+
+    Node() {}
+    Node(const T& val) : next(nullptr), data(val) {}
+    Node(T&& val) : next(nullptr), data(forward<T>(val)) {}
 
     void print()
     {
         Node *node = this;
         while (node != NULL)
         {
-            printf("%s ", node->data);
+            cout << node->data << " ";
             node = node->next;
         }
-        printf("\n");
+        cout << endl;
+    }
+};
+
+template <typename T>
+class SinglyLinkedList
+{
+public:
+    Node<T> *head = NULL;
+
+    SinglyLinkedList() {}
+
+    template <class ... Args>
+    SinglyLinkedList(Args ... args)
+    {
+        const int size = sizeof...(args);
+        T a[size] = {args...};
+        for (int i = size - 1; i >= 0; i--)
+        {
+            this->push(a[i]);
+        }
+    }
+
+    void push(T data)
+    {
+        Node<T> *new_node = new Node<T>();
+        new_node->data = data;
+        new_node->next = head;
+        head = new_node;
+    }
+    void insert(Node<T> *prev, T data)
+    {
+        Node<T> *new_node = new Node<T>();
+        new_node->data = data;
+        new_node->next = prev->next;
+        prev->next = new_node;
+    }
+    void append(T data)
+    {
+        Node<T> *new_node = new Node<T>();
+        new_node->data = data;
+        new_node->next = NULL;
+        Node<T> *last = head;
+        while (last->next != NULL)
+        {
+            last = last->next;
+        }
+        last->next = new_node;
     }
 };
 
 int main()
 {
-    Node *head = new Node();
-    Node *second = new Node();
-    Node *third = new Node();
+    SinglyLinkedList<int> int_list = SinglyLinkedList<int>(69, 420);
+    int_list.head->print();
 
-    head->data = "head";
-    head->next = second;
-
-    second->data = "second";
-    second->next = third;
-
-    third->data = "third";
-    third->next = NULL;
-
-    head->print();
+    SinglyLinkedList<const char *> str_list = SinglyLinkedList<const char *>("dungeon", "master");
+    str_list.head->print();
 
     return 0;
 }
