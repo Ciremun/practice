bfbc39df2db73eed576b0bc92cd9f1dddd05de77
Ciremun <Ciremun@yandex.ru>
2021-04-25 04:00:10 +0300

    list with multiple types

--- linked-list.cpp
+++ linked-list.cpp
@@ -1,40 +1,101 @@
 #include <iostream>
+#include <variant>
 
 using namespace std;
 
-template <typename T>
+template<typename T>
+using base_type = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
+
+enum class type
+{
+    int_type,
+    float_type,
+    string_type,
+    bool_type
+};
+
 class Node
 {
 public:
-    T data;
+    using Data = variant<int, float, string, bool>;
+    Data data;
     Node *next;
+    type t;
 
     Node() {}
-    Node(const T &val, Node<T> *nxt) : next(nxt), data(val) {}
-    Node(T &&val, Node<T> *nxt) : next(nxt), data(forward<T>(val)) {}
+
+    template <typename T>
+    Node(const T &val, Node *nxt) : next(nxt), data(Data(val))
+    {
+        set_type(val);
+    }
+
+    template <typename T>
+    Node(T &&val, Node *nxt) : next(nxt), data(forward<T>(val))
+    {
+        set_type(val);
+    }
+
+    template <typename T>
+    void set_type(const T&val)
+    {
+        if (is_same_v<T, int>)
+        {
+            t = type::int_type;
+        }
+        else if (is_same_v<T, float>)
+        {
+            t = type::float_type;
+        }
+        else if (is_same_v<T, string>)
+        {
+            t = type::string_type;
+        }
+        else if (is_same_v<T, bool>)
+        {
+            t = type::bool_type;
+        }
+    }
+
+    template <typename F>
+    bool value(F callback) const
+    {
+        switch (this->t)
+        {
+            case type::int_type:
+                return callback(get<int>(this->data));
+            case type::float_type:
+                return callback(get<float>(this->data));
+            case type::string_type:
+                return callback(get<string>(this->data));
+            case type::bool_type:
+                return callback(get<bool>(this->data));
+            default:
+                return false;
+        }
+    }
 
     void print()
     {
         Node *node = this;
         while (node != NULL)
         {
-            cout << node->data << " ";
+            node->value([](auto &val) -> bool { cout << val << " "; return true; });
             node = node->next;
         }
         cout << endl;
     }
 };
 
-template <typename T>
 class SinglyLinkedList
 {
 public:
-    Node<T> *head = NULL;
+    Node *head = NULL;
 
     SinglyLinkedList() {}
     ~SinglyLinkedList()
     {
-        Node<T> *next = NULL;
+        Node *next = NULL;
         while (head != NULL)
         {
             next = head->next;
@@ -49,31 +110,34 @@ public:
         ([&](auto &&input) { this->append(input); }(args), ...);
     }
 
+    template <typename T>
     void push(T data)
     {
-        Node<T> *new_node = new Node<T>(move(data), head);
+        Node *new_node = new Node(move(data), head);
         head = new_node;
     }
 
-    void insert(Node<T> *prev, T data)
+    template <typename T>
+    void insert(Node *prev, T data)
     {
         if (prev == NULL)
         {
             return;
         }
-        Node<T> *new_node = new Node<T>(move(data), prev->next);
+        Node *new_node = new Node(move(data), prev->next);
         prev->next = new_node;
     }
 
+    template <typename T>
     void append(T data)
     {
-        Node<T> *new_node = new Node<T>(move(data), NULL);
+        Node *new_node = new Node(move(data), NULL);
         if (head == NULL)
         {
             head = new_node;
             return;
         }
-        Node<T> *last = head;
+        Node *last = head;
         while (last->next != NULL)
         {
             last = last->next;
@@ -81,17 +145,38 @@ public:
         last->next = new_node;
     }
 
+    template <typename T>
     void remove(T data)
     {
-        Node<T> *temp = head;
-        if (temp != NULL && temp->data == data)
+        Node *temp = head;
+        if (temp != NULL && temp->value([&data](auto &val) -> bool
+        {
+            if constexpr (is_same_v<T, base_type<decltype(val)>>)
+            {
+                return val == data;
+            }
+            else
+            {
+                return false;
+            }
+        }))
         {
             head = temp->next;
             delete temp;
             return;
         }
-        Node<T> *prev = NULL;
-        while (temp != NULL && temp->data != data)
+        Node *prev = NULL;
+        while (temp != NULL && temp->value([&data](auto &val) -> bool
+        {
+            if constexpr (is_same_v<T, base_type<decltype(val)>>)
+            {
+                return val != data;
+            }
+            else
+            {
+                return true;
+            }
+        }))
         {
             prev = temp;
             temp = temp->next;
@@ -110,7 +195,7 @@ public:
         {
             return;
         }
-        Node<T> *temp = head;
+        Node *temp = head;
         if (idx == 0)
         {
             head = temp->next;
@@ -125,16 +210,16 @@ public:
         {
             return;
         }
-        Node<T> *new_next = temp->next->next;
+        Node *new_next = temp->next->next;
         delete temp->next;
         temp->next = new_next;
     }
 
     void reverse()
     {
-        Node<T> *prev = NULL;
-        Node<T> *current = head;
-        Node<T> *next = NULL;
+        Node *prev = NULL;
+        Node *current = head;
+        Node *next = NULL;
         while (current != NULL)
         {
             next = current->next;
@@ -153,7 +238,7 @@ public:
     size_t size()
     {
         size_t count = 0;
-        Node<T> *current = head;
+        Node *current = head;
         while (current != NULL)
         {
             current = current->next;
@@ -162,12 +247,23 @@ public:
         return count;
     }
 
+    template <typename T>
     bool contains(T data)
     {
-        Node<T> *current = head;
+        Node *current = head;
         while (current != NULL)
         {
-            if (current->data == data)
+            if (current->value([&data](auto &val) -> bool
+            {
+                if constexpr (is_same_v<T, base_type<decltype(val)>>)
+                {
+                    return val == data;
+                }
+                else
+                {
+                    return false;
+                }
+            }))
             {
                 return true;
             }
@@ -176,13 +272,13 @@ public:
         return false;
     }
 
-    const Node<T> *operator[](size_t idx) const
+    const Node *operator[](size_t idx) const
     {
         if (idx == 0)
         {
             return head;
         }
-        Node<T> *temp = head;
+        Node *temp = head;
         for (size_t i = 1; temp != NULL && i <= idx; i++)
         {
             temp = temp->next;
@@ -190,10 +286,10 @@ public:
         return temp;
     }
 
-    bool operator==(const SinglyLinkedList<T> &other)
+    bool operator==(const SinglyLinkedList &other)
     {
-        Node<T> *left = head;
-        Node<T> *right = other.head;
+        Node *left = head;
+        Node *right = other.head;
         while (left != NULL && right != NULL)
         {
             if (left->data != right->data)
@@ -210,7 +306,7 @@ public:
         return true;
     }
 
-    bool operator!=(const SinglyLinkedList<T> &other)
+    bool operator!=(const SinglyLinkedList &other)
     {
         return !(*this == other);
     }
@@ -218,44 +314,52 @@ public:
 
 int main()
 {
-    SinglyLinkedList<int> *int_list = new SinglyLinkedList<int>(69, 420, 1337);
-    int_list->remove(420);
-    int_list->remove(1337);
-    int_list->print();
-    cout << "size: " << int_list->size() << endl;
-    cout << "contains `420`: " << int_list->contains(420) << endl;
-    cout << "int_list[0]: " << (*int_list)[0]->data << endl;
+    SinglyLinkedList *list_1 = new SinglyLinkedList(69, 420.5f, string("dungeon master"));
+    list_1->remove(420);
+    list_1->remove(1337);
+    list_1->print();
+    cout << "size: " << list_1->size() << endl;
+    cout << "contains `420`: " << list_1->contains(420) << endl;
+    (*list_1)[0]->value([](auto &val){ cout << "list_1[0]: " <<  val << endl; return true; });
     cout << endl;
 
-    SinglyLinkedList<string> *str_list = new SinglyLinkedList<string>("boss", "dungeon");
-    str_list->insert(str_list->head, "slave");
-    str_list->remove_at(1);
-    str_list->remove("boss");
-    str_list->append("master");
-    str_list->print();
-    cout << "size: " << str_list->size() << endl;
-    cout << "contains `master`: " << str_list->contains("master") << endl;
-    cout << "str_list[1]: " << (*str_list)[1]->data << endl;
+    SinglyLinkedList *list_2 = new SinglyLinkedList(string("boss"), string("dungeon"));
+    list_2->insert(list_2->head, string("slave"));
+    list_2->remove_at(1);
+    list_2->remove(string("boss"));
+    list_2->append(string("master"));
+    list_2->print();
+    cout << "size: " << list_2->size() << endl;
+    cout << "contains `master`: " << list_2->contains(string("master")) << endl;
+    (*list_2)[1]->value([](auto &val){ cout << "list_2[1]: " <<  val << endl; return true; });
     cout << endl;
 
-    SinglyLinkedList<float> *float_list = new SinglyLinkedList<float>(69.5f, 420.5f, 1337.5f);
-    cout << "float_list: \t";
-    float_list->print();
-    float_list->reverse();
+    SinglyLinkedList *list_3 = new SinglyLinkedList(false, string("second"), 3, 3.5f);
+    cout << "list_3: \t";
+    list_3->print();
+    list_3->reverse();
     cout << "reversed: \t";
-    float_list->print();
+    list_3->print();
+    cout << endl;
 
-    SinglyLinkedList<bool> *bool_list = new SinglyLinkedList<bool>(true, true, false);
-    SinglyLinkedList<bool> *bool_list_2 = new SinglyLinkedList<bool>(true, false, false);
+    SinglyLinkedList *bool_list = new SinglyLinkedList(true, true, false);
+    SinglyLinkedList *bool_list_2 = new SinglyLinkedList(true, false, false);
+
+    bool_list->print();
+    bool_list_2->print();
 
     cout << "bool_list != bool_list_2: " << (*bool_list != *bool_list_2) << endl;
     bool_list_2->insert(bool_list_2->head, true);
     bool_list_2->remove_at(2);
+
+    bool_list->print();
+    bool_list_2->print();
+
     cout << "bool_list == bool_list_2: " << (*bool_list == *bool_list_2) << endl;
 
-    delete int_list;
-    delete str_list;
-    delete float_list;
+    delete list_1;
+    delete list_2;
+    delete list_3;
     delete bool_list;
     delete bool_list_2;
 
